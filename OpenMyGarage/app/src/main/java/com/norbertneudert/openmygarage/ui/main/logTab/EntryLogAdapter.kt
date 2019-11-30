e4bbc66c29ac7bd52824510e456f1fe0f5d46e50
Norbert Neudert <norbertneudert@gmail.com>
2019-11-30 19:52:32 +0100

    Minor changes to EntryLogAdapter

--- OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/ui/main/logTab/EntryLogAdapter.kt
+++ OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/ui/main/logTab/EntryLogAdapter.kt
@@ -1,12 +1,19 @@
 package com.norbertneudert.openmygarage.ui.main.logTab
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.norbertneudert.openmygarage.R
 import com.norbertneudert.openmygarage.TextItemViewHolder
 import com.norbertneudert.openmygarage.database.EntryLog
 
 class EntryLogAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
     var data = listOf<EntryLog>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     override fun getItemCount(): Int {
         return data.size
@@ -14,10 +21,13 @@ class EntryLogAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
 
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.plate
+        var text: String = item.plate.plus(item.entryTime.toString())
+        holder.textView.text = text
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
+        return TextItemViewHolder(view)
     }
 }
