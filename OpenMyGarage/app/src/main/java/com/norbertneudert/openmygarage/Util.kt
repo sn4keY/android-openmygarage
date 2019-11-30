373599ea2d1cf1fcb3e05a67fa112bbb726ac7c4
Norbert Neudert <norbertneudert@gmail.com>
2019-11-30 17:52:37 +0100

    Added Util class

---
+++ OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/Util.kt
@@ -0,0 +1,6 @@
+package com.norbertneudert.openmygarage
+
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TextItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
