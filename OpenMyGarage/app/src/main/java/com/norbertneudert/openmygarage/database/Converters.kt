aa9b61de7c3440487e36c56151ae7f406a91d747
Norbert Neudert <norbertneudert@gmail.com>
2019-11-30 16:17:59 +0100

    Added converting between Outcome and int

--- OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/database/Converters.kt
+++ OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/database/Converters.kt
@@ -13,4 +13,24 @@ class Converters {
     fun dateToTimestamp(date: Date?): Long? {
         return date?.time?.toLong()
     }
+
+    @TypeConverter
+    fun intToOutcome(value: Int?): Outcome? {
+        when(value) {
+            1 -> return Outcome.OPEN
+            2 -> return Outcome.NOTIFY
+            3 -> return Outcome.REFUSE
+            else -> return Outcome.NOTIFY
+        }
+    }
+
+    @TypeConverter
+    fun outcomeToInt(outcome: Outcome?): Int? {
+        when(outcome) {
+            Outcome.OPEN -> return 1
+            Outcome.NOTIFY -> return 2
+            Outcome.REFUSE -> return 3
+            else -> return 2
+        }
+    }
 }
