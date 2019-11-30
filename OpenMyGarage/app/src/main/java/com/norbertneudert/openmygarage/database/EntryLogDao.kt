18a40e51bea2e3f5538c8da218537a6cef37908c
Norbert Neudert <norbertneudert@gmail.com>
2019-11-30 19:51:26 +0100

    Updated EntryLogDao to return LiveData

--- OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/database/EntryLogDao.kt
+++ OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/database/EntryLogDao.kt
@@ -1,14 +1,22 @@
 package com.norbertneudert.openmygarage.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import java.util.*
 
 @Dao
 interface EntryLogDao {
     @Query("SELECT * FROM entry_log_table ORDER BY entry_time DESC")
-    fun getEntryLogs(): List<EntryLog>
+    fun getEntryLogs(): LiveData<List<EntryLog>>
 
     @Query("SELECT * FROM entry_log_table WHERE :dateFrom < entry_time < :dateUntil ORDER BY entry_time DESC")
-    fun getEntryLogsBetween(dateFrom: Date, dateUntil: Date): List<EntryLog>
+    fun getEntryLogsBetween(dateFrom: Date, dateUntil: Date): LiveData<List<EntryLog>>
+
+    @Insert
+    fun insert(entryLog: EntryLog)
+
+    @Query("DELETE FROM entry_log_table")
+    fun clear()
 }
