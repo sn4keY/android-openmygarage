984ec91e34e20a52251c6a10f9fd8e78d598a8fd
Norbert Neudert <norbertneudert@gmail.com>
2019-12-01 18:50:39 +0100

    Added LogsViewHolder to use in EntryLogAdapter

--- OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/Util.kt
+++ OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/Util.kt
@@ -5,3 +5,14 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
+class LogsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var plateTextView: TextView
+    var timeTextView: TextView
+    var outcomeTextView: TextView
+
+    init {
+        plateTextView = itemView.findViewById(R.id.plate_logs_tw)
+        timeTextView = itemView.findViewById(R.id.time_logs_tw)
+        outcomeTextView = itemView.findViewById(R.id.outcome_logs_tw)
+    }
+}
