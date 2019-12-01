7f31fa36e2a00b93f86b642fb0cd5df4ad3885cc
Norbert Neudert <norbertneudert@gmail.com>
2019-12-01 18:51:02 +0100

    Added PlatesViewHolder to use in PlatesAdapter

--- OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/Util.kt
+++ OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/Util.kt
@@ -16,3 +16,19 @@ class LogsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         outcomeTextView = itemView.findViewById(R.id.outcome_logs_tw)
     }
 }
+
+class PlatesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var nameTextView: TextView
+    var plateTextView: TextView
+    var outcomeTextView: TextView
+    var editButton: Button
+    var deleteButton: Button
+
+    init {
+        nameTextView = itemView.findViewById(R.id.name_plates_tw) as TextView
+        plateTextView = itemView.findViewById(R.id.plate_plates_tw) as TextView
+        outcomeTextView = itemView.findViewById(R.id.outcome_plates_tw) as TextView
+        editButton = itemView.findViewById(R.id.edit_button) as Button
+        deleteButton = itemView.findViewById(R.id.delete_button) as Button
+    }
+}
