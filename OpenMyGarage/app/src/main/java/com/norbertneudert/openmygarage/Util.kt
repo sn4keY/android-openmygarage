783e745746a9c4cfcdda3cf009bac2e73f6113c1
Norbert Neudert <norbertneudert@gmail.com>
2019-12-01 18:50:01 +0100

    Removed TextItemView because its no longer used

--- OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/Util.kt
+++ OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/Util.kt
@@ -1,8 +1,7 @@
 package com.norbertneudert.openmygarage
 
 import android.view.View
-import android.widget.LinearLayout
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class TextItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
