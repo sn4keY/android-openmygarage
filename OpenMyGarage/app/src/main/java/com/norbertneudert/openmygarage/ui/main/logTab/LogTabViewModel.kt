07c24f2648373e69ce34b87c8e59f2563da51c3a
Norbert Neudert <norbertneudert@gmail.com>
2019-12-01 19:08:18 +0100

    Added more EntryLogs to the database

--- OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/ui/main/logTab/LogTabViewModel.kt
+++ OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/ui/main/logTab/LogTabViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.norbertneudert.openmygarage.database.EntryLog
 import com.norbertneudert.openmygarage.database.EntryLogDao
+import com.norbertneudert.openmygarage.database.Outcome
 import kotlinx.coroutines.*
 
 class LogTabViewModel(val database: EntryLogDao, application: Application) : AndroidViewModel(application) {
@@ -37,8 +38,16 @@ class LogTabViewModel(val database: EntryLogDao, application: Application) : And
 
     private suspend fun populateEntryLogs() {
         withContext(Dispatchers.IO) {
-            database.insert(EntryLog(plate = "ABC-123"))
+            database.insert(EntryLog(plate = "ABC-123", outcome = Outcome.OPEN))
             database.insert(EntryLog(plate = "XYZ-123"))
+            database.insert(EntryLog(plate = "UJA-462"))
+            database.insert(EntryLog(plate = "PIS-823", outcome = Outcome.OPEN))
+            database.insert(EntryLog(plate = "BSR-312"))
+            database.insert(EntryLog(plate = "ZAK-012"))
+            database.insert(EntryLog(plate = "MKA-721"))
+            database.insert(EntryLog(plate = "LUC-666", outcome = Outcome.OPEN))
+            database.insert(EntryLog(plate = "GOD-420"))
+            database.insert(EntryLog(plate = "UAE-999"))
         }
     }
 }
