656ad4749ba83c7ea7d37927e5e86d8b135ffb63
Norbert Neudert <norbertneudert@gmail.com>
2019-12-05 15:37:39 +0100

    Removed unneccessary imports

--- OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/MainActivity.kt
+++ OpenMyGarage/app/src/main/java/com/norbertneudert/openmygarage/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.norbertneudert.openmygarage
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import com.norbertneudert.openmygarage.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
